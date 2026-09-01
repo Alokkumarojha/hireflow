65fa08ad46df6783d98356852a2d92e9ee1e4f2a
Alok kumar ojha <aojha105@gmail.com>
2026-09-01 15:34:54 +0530

    feat: add Prisma and Neon database

---
+++ prisma/migrations/20260901095620_add_user_model/migration.sql
@@ -0,0 +1,21 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('CANDIDATE', 'EMPLOYER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "clerkId" TEXT NOT NULL,
+    "name" TEXT,
+    "email" TEXT NOT NULL,
+    "role" "UserRole" NOT NULL DEFAULT 'CANDIDATE',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_clerkId_key" ON "User"("clerkId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
